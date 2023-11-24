db1a33eda0fe80dd54ebf474eba2be1e4dd24da8
lightningfast07 <108779360+lightningfast07@users.noreply.github.com>
2023-11-24 12:49:09 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(mongodb+srv://dibya567darsan:YG4iJRoTyrFuk5Yr@cluster2.ilwbssa.mongodb.net/?retryWrites=true&w=majority);
+    const conn = await mongoose.connect('mongodb+srv://dibya567darsan:YG4iJRoTyrFuk5Yr@cluster2.ilwbssa.mongodb.net/?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
